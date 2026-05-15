62c9fb82e6998f0322ed7603701ea895cc5ff7e5
Divyansh <bhardwajdivyansh2002@gmail.com>
2026-05-15 23:21:15 +0530

    Initial Commit

---
+++ Quest For Duskara/Quest_For_DuskaraApp.swift
@@ -0,0 +1,17 @@
+//
+//  Quest_For_DuskaraApp.swift
+//  Quest For Duskara
+//
+//  Created by db on 15/05/26.
+//
+
+import SwiftUI
+
+@main
+struct Quest_For_DuskaraApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
