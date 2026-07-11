e61d993e4684654a171dcd16ea11d4f455b84894
Divyansh <bhardwajdivyansh2002@gmail.com>
2026-07-11 17:09:47 +0530

    feat: add server-authoritative Firebase command replication

--- firebase/functions/src/index.ts
+++ firebase/functions/src/index.ts
@@ -1,6 +1,9 @@
 import {onCall} from "firebase-functions/v2/https";
 import {cancelMatchmakingHandler, joinMatchmakingHandler} from "./matchmaking.js";
 import {createRoomHandler, joinRoomHandler, leaveRoomHandler, setReadyHandler, startRoomHandler} from "./roomService.js";
+import {fetchCheckpointHandler, submitGameActionHandler} from "./gameReducer.js";
+import {onSchedule} from "firebase-functions/v2/scheduler";
+import {advanceDueRooms} from "./dayAdvance.js";
 
 const options = {region: "asia-south1", enforceAppCheck: process.env.FUNCTIONS_EMULATOR !== "true"};
 export const createRoom = onCall(options, createRoomHandler);
@@ -10,3 +13,6 @@ export const setLobbyReady = onCall(options, setReadyHandler);
 export const startRoom = onCall(options, startRoomHandler);
 export const joinMatchmaking = onCall(options, joinMatchmakingHandler);
 export const cancelMatchmaking = onCall(options, cancelMatchmakingHandler);
+export const submitGameAction = onCall(options, submitGameActionHandler);
+export const fetchCheckpoint = onCall(options, fetchCheckpointHandler);
+export const advanceDays = onSchedule({region: "asia-south1", schedule: "every 1 minutes"}, advanceDueRooms);
